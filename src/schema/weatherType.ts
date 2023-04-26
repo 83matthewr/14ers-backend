dd6935ba204b83b475025a2291895399d787009e
Matthew Roberts <83matthewr@gmail.com>
2023-04-26 18:40:26 -0400

    Added weatherType

---
+++ src/schema/weatherType.ts
@@ -0,0 +1,21 @@
+import graphql from 'graphql';
+const {
+  GraphQLObjectType,
+  GraphQLFloat,
+  GraphQLString,
+  GraphQLInt,
+} = graphql;
+
+const WeatherType = new GraphQLObjectType({
+  name:  'WeatherType',
+  fields: () => ({
+    temperature: { type: GraphQLFloat },
+    feelslike: { type: GraphQLFloat },
+    condition: { type: GraphQLString },
+    windspeed: { type: GraphQLFloat },
+    winddirection: { type: GraphQLString },
+    humidity: { type: GraphQLInt }
+  })
+});
+
+export default WeatherType;
